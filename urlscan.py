7fd9cf3635eff309d090a62e3fa3acac1fa38f0b
x1mus <MaxBresil@outlook.com>
2021-03-01 15:31:03 +0100

    Initial commit
    Initial commit

---
+++ urlscan.py
@@ -0,0 +1,47 @@
+import requests, json
+
+class UrlScan():
+	def __init__(self, apikey="", url="", public=False):
+		assert len(apikey) > 0, "API key must be defined"
+		assert len(url) > 0, "Url must be defined"
+		assert type(public) == type(True), "Public must be a boolean"
+		self.apikey = apikey
+		self.url = url
+		self.public = public
+		self.scanid = None
+
+	def submit(self):
+		header = {'API-Key': self.apikey}
+		req = {"url": self.url}
+
+		if self.public:
+			req['public'] = "on"
+
+		s = requests.post("https://urlscan.io/api/v1/scan/", data=req, headers=header)
+
+		if s.status_code == 200:
+			self.scanid = json.loads(s.text)['uuid']
+			print(self.scanid)
+		else:
+			s.raise_for_status()
+
+	def checkStatus(self):
+		header = {'API-Key': self.apikey}
+
+		r = requests.get("https://urlscan.io/api/v1/result/%s/" % self.scanid, headers=header)
+
+		if r.status_code == 404:
+			r.raise_for_status()
+
+		return r
+
+	def getScreenshot(self):
+		self.checkStatus()
+		header = {'API-Key': self.apikey}
+
+		screen = requests.get("https://urlscan.io/screenshots/%s.png" % self.scanid, headers=header)
+
+		if screen.status_code == 200:
+			return screen.content
+		else:
+			screen.raise_for_status()
---
+++ urlscanio.py
@@ -0,0 +1,79 @@
+from time import sleep
+from urlscan import *
+
+from assemblyline_v4_service.common.base import ServiceBase
+from assemblyline_v4_service.common.result import Result, ResultSection, BODY_FORMAT
+
+class UrlScanIo(ServiceBase):
+	def __init__(self, config=None):
+		super(UrlScanIo, self).__init__(config)
+
+	def start(self):
+		self.log.debug("Urlscan.io service started")
+
+	def stop(self):
+		self.log.debug("Urlscan.io service ended")
+
+	def wait_processing(self, u):
+		completed = 0
+
+		# Checks if there's a result on the API page
+		while not completed:
+			try:
+				response = u.checkStatus()
+				completed = 1
+			except requests.exceptions.HTTPError:
+				completed = 0
+
+		# If there's a result but this one is still being processed we wait
+		while "notdone" in response.json().values():
+			sleep(1)
+			response = u.checkStatus()
+
+		return response
+
+	def execute(self, request):
+		result = Result()
+		url = request.file_name
+		api_key = request.get_param("api_key")
+		public = request.get_param("public")
+
+		u = UrlScan(apikey=api_key, url=url, public=public)
+		u.submit()
+
+		# We need to wait for the API to process our request
+		response = self.wait_processing(u)
+
+		# We get the response parts that we want and merge them all together
+		report = {
+			**response.json()["verdicts"]["overall"],
+			**response.json()["lists"],
+			**response.json()["page"]
+		}
+
+		# We convert the "certicates" section from a list of dictionnaries to a dictionnary of lists
+		certificates = report.pop("certificates")
+		certificates = {k: [dic[k] for dic in certificates] for k in certificates[0]}
+
+		# We add the converted section to the report
+		report = {**report, **certificates}
+
+		# We create the KEY_VALUE section to add the report to the result page
+		kv_section = ResultSection("Urlscan.io report", body_format=BODY_FORMAT.KEY_VALUE, body=json.dumps(report))
+
+		for domain in report["domains"]:
+			kv_section.add_tag("network.static.domain", domain.strip())
+
+		result.add_section(kv_section)
+
+		# We get the preview of the website
+		screenshot = u.getScreenshot()
+		with open(self.working_directory + "/preview.png", "wb") as ofile:
+			ofile.write(screenshot)
+
+		# Adding the preview on the result page
+		url_section = ResultSection('Urlscan.io website screenshot', body_format=BODY_FORMAT.URL, body=json.dumps({"name": "The preview is also available here !", "url": response.json()["task"]["screenshotURL"]}))
+		result.add_section(url_section)
+		request.add_extracted(self.working_directory + "/preview.png", "preview.png", "Here\'s the preview of the site")
+
+		request.result = result
